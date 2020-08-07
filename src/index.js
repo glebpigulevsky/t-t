a332d67c5fdeb4fd2f9d1ef6f37dbce81394e057
CasperCarver <hleb.piguleuski@gmail.com>
2020-08-07 18:40:42 +0300

    feat: implement standart functions in index.js

--- src/index.js
+++ src/index.js
@@ -5,27 +5,105 @@ const phoneBanner = document.querySelector('.phone_banner');
 const fileBanner = document.querySelector('.input-file');
 const commentBanner = document.querySelector('.comment_banner');
 
-document.addEventListener('click', (e) => {
-  if (e.target === document.querySelector('.inputButton')) {
+const nameBannerAbout = document.querySelector('.name_banner_about_mobile');
+const phoneBannerAbout = document.querySelector('.phone_banner_about_mobile');
+const fileBannerAbout = document.querySelector('.input-file_about_mobile');
+const commentBannerAbout = document.querySelector(
+  '.comment_banner_about_mobile'
+);
+
+const nameBannerAboutScreen = document.querySelector(
+  '.name_banner_about_screen'
+);
+const phoneBannerAboutScreen = document.querySelector(
+  '.phone_banner_about_screen'
+);
+const fileBannerAboutScreen = document.querySelector('.fileBannerAboutScreen');
+const commentBannerAboutScreen = document.querySelector(
+  '.comment_banner_about_screen'
+);
+
+const closeModal = document.querySelector('.modal_close');
+const closeErrorFromModal = document.querySelector('.error_form_close');
+
+document.addEventListener('click', async (e) => {
+  if (e.target === document.querySelector('.input_button-contacts_mobile')) {
     e.preventDefault();
-    sendFormTelegram();
+    if (phoneBannerAbout.value === '' || commentBannerAbout.value === '') {
+      document.querySelector('.error_form').classList.remove('hidden');
+    } else {
+      const formD = new FormData();
+      await formD.append('photo', fileBannerAbout.files[0]);
+      await formD.append('name', nameBannerAbout.value);
+      await formD.append('phone', phoneBannerAbout.value);
+      await formD.append('comment', commentBannerAbout.value);
+      sendFormTelegram(formD);
+      nameBannerAbout.value = '';
+      phoneBannerAbout.value = '';
+      commentBannerAbout.value = '';
+      fileBannerAbout.value = '';
+    }
+  }
+});
+document.addEventListener('click', async (e) => {
+  if (e.target === document.querySelector('.input_button-contacts_screen')) {
+    e.preventDefault();
+    if (
+      phoneBannerAboutScreen.value === '' ||
+      commentBannerAboutScreen.value === ''
+    ) {
+      document.querySelector('.error_form').classList.remove('hidden');
+    } else {
+      const formD = new FormData();
+      await formD.append('photo', fileBannerAboutScreen.files[0]);
+      await formD.append('name', nameBannerAboutScreen.value);
+      await formD.append('phone', phoneBannerAboutScreen.value);
+      await formD.append('comment', commentBannerAboutScreen.value);
+      sendFormTelegram(formD);
+      nameBannerAboutScreen.value = '';
+      phoneBannerAboutScreen.value = '';
+      commentBannerAboutScreen.value = '';
+      fileBannerAboutScreen.value = '';
+    }
   }
 });
 
-async function sendFormTelegram() {
-  const formD = new FormData();
-  await formD.append('photo', fileBanner.files[0]);
-  await formD.append('name', nameBanner.value);
-  await formD.append('phone', phoneBanner.value);
-  await formD.append('comment', commentBanner.value);
+function openModal() {
+  document.querySelector('.modal__overlay').classList.remove('hidden');
+}
 
+closeModal.addEventListener('click', () => {
+  document.querySelector('.modal__overlay').classList.add('hidden');
+});
+closeErrorFromModal.addEventListener('click', () => {
+  document.querySelector('.error_form').classList.add('hidden');
+});
+
+document.addEventListener('click', async (e) => {
+  if (e.target === document.querySelector('.inputButton')) {
+    e.preventDefault();
+    if (phoneBanner.value === '' && commentBanner.value === '') {
+      document.querySelector('.error_form').classList.remove('hidden');
+    } else {
+      const formD = new FormData();
+      await formD.append('photo', fileBanner.files[0]);
+      await formD.append('name', nameBanner.value);
+      await formD.append('phone', phoneBanner.value);
+      await formD.append('comment', commentBanner.value);
+      sendFormTelegram(formD);
+      nameBanner.value = '';
+      phoneBanner.value = '';
+      commentBanner.value = '';
+      fileBanner.value = '';
+    }
+  }
+});
+
+async function sendFormTelegram(form) {
   try {
-    const res = await axios.post('/', formD);
+    const res = await axios.post('/', form);
     console.log(res.data);
-    nameBanner.value = '';
-    phoneBanner.value = '';
-    commentBanner.value = '';
-    fileBanner.value = '';
+
     const url =
       'https://api.telegram.org/bot1233667834:AAHz_bng0VaZaI8UxLH6QXHpBC8wU-04WIY/sendMessage?chat_id=-440657814&text=';
     const text =
@@ -36,47 +114,16 @@ async function sendFormTelegram() {
       '%0AКомментарий: ' +
       res.data.text;
     let sendedUrl = url + text;
-
     await fetch(sendedUrl);
     const photoLink =
       'https://api.telegram.org/bot1233667834:AAHz_bng0VaZaI8UxLH6QXHpBC8wU-04WIY/sendPhoto?chat_id=-440657814&photo=';
     await fetch(photoLink + res.data.postImage);
+    openModal();
   } catch (err) {
     console.log(err);
   }
 }
 
-// async function sendFormTelegram(){
-
-//     let files = fileBanner.files;
-
-//     let img = window.URL.createObjectURL(files[0]);
-
-//     console.log(img);
-//   const url = "https://api.telegram.org/bot1233667834:AAHz_bng0VaZaI8UxLH6QXHpBC8wU-04WIY/sendMessage?chat_id=-440657814&text=";
-//   const text = "Заказ от: " + nameBanner.value + "%0AТелефон: " + phoneBanner.value + "%0AКомментарий: " + commentBanner.value;
-
-//   let sendedUrl = url+text;
-//   await fetch(sendedUrl);
-//   nameBanner.value = '';
-//   phoneBanner.value = '';
-//   commentBanner.value = '';
-
-// };
-
-// fileBanner.addEventListener("change", async function(event) {
-//     Когда происходит изменение элементов управления, значит появились новые файлы
-
-//     var i = 0,
-//         files = fileBanner.files,
-//         len = files.length;
-//     let img = window.URL.createObjectURL(files[0]);
-//     img = img.slice(5, img.length);
-//     console.log(img);
-//     const urlFile = "https://api.telegram.org/file/bot1233667834:AAHz_bng0VaZaI8UxLH6QXHpBC8wU-04WIY/"
-//     let sendedurlFile = urlFile+img;
-//     const res = await fetch(sendedurlFile);
-//     const data = await res.json();
-//     console.log(data);
-
-// }, false);
+function addModal() {
+  let modalDiv = document.querySelector('.modal__overlay');
+}
